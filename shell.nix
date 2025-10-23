c17773c616d093cc2bf90b6bae2b02af3795a252
antenomy <112831702+antenomy@users.noreply.github.com>
2025-10-24 01:18:21 +0200

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -9,37 +9,7 @@ pkgs.mkShell {
     export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
       pkgs.stdenv.cc.cc
     ]}
-    echo "Activated work-hm-project dev environment."
+    echo "Activated project-fashion-ragsearch dev environment."
     export PYTHONNOUSERSITE="true"
   '';
 }
-
-    #   nativeBuildInputs = with pkgs; [
-    #playwright-driver.browsers
-  #];
-    # mkdir -p .env/playwright-browsers
-    # ln -sf ${pkgs.playwright-driver.browsers} .env/playwright-browsers
-    # export PLAYWRIGHT_BROWSERS_PATH="$(pwd)/.env/playwright-browsers"
-
-    # export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
-
-  #   export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
-  #     pkgs.stdenv.cc.cc
-  #     pkgs.glib
-  #     pkgs.gtk3
-  #     pkgs.nss
-  #     pkgs.nspr
-  #     pkgs.dbus
-  #     pkgs.at-spi2-core
-  #     pkgs.xdg-utils
-  #     pkgs.libxkbcommon
-  #     pkgs.cairo
-  #     pkgs.pango
-  #     pkgs.freetype
-  #     pkgs.fontconfig
-  #     pkgs.cups
-  #     pkgs.expat
-  #     pkgs.xorg.libxcb
-  #     pkgs.xorg.libX11
-  #     pgkgs.pkgs.xorg.libXcomposite
-  #   ]}:$LD_LIBRARY_PATH
