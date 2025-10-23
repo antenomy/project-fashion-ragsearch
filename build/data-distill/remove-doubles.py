2966b59a487b918c597f0d1b84583572a2c40ceb
antenomy <112831702+antenomy@users.noreply.github.com>
2025-10-24 01:24:34 +0200

    Delete build directory

--- build/data-distill/remove-doubles.py
+++
@@ -1,9 +0,0 @@
-file_name = 'round-2.txt' #input()
-
-with open(file_name, 'r') as file:
-    product_list = [line.strip() for line in file]
-
-    simplified_product_list = list(set(product_list))
-
-with open(file_name, 'w') as file:
-    file.write(',\n'.join(f"{item}" for item in simplified_product_list))
